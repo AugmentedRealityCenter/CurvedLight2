0326e3c28a0a15308a9fde2e3995f6acb5e36674
cluelesswalnut <ernst.sukram@gmail.com>
2016-04-04 18:40:26 -0400

    added a variable to track eggs collected

--- ProjectTangoCurvedLight/Assets/spawnEggs.cs
+++ ProjectTangoCurvedLight/Assets/spawnEggs.cs
@@ -9,14 +9,22 @@ public class spawnEggs : MonoBehaviour {
 	private int pointIndex = -1;
 	private int lastPoint = -1;
 
+	private int eggsCollected = -1;
+
 	void Start() {
 			spawnPoints = GameObject.FindGameObjectsWithTag("SpawnPoint");
 
 		spawnEgg ();
+		InvokeRepeating("collected", 0, 5.0F);
+	}
+
+	void collected(){
+		AndroidHelper.ShowAndroidToastMessage(""+eggsCollected);
 	}
 
 	void spawnEgg()
 	{
+		++eggsCollected;
 		if (spawnPoints.Length > 1) {
 			while (pointIndex == lastPoint) {
 				pointIndex = Random.Range (0, spawnPoints.Length);
