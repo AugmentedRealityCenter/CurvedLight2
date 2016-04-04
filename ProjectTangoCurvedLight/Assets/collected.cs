1cb97d24830ae8d1d47791ff38e639802c406606
cluelesswalnut <ernst.sukram@gmail.com>
2016-04-04 12:23:28 -0400

    added spawning of eggs
    an egg spawns at one of the spawnPoint in the scene every time the
    previously spawned egg is collected

---
+++ ProjectTangoCurvedLight/Assets/collected.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class collected : MonoBehaviour {
+
+	private GameObject spawner;
+
+	// Use this for initialization
+	void Start () {
+		spawner = GameObject.FindGameObjectWithTag ("Spawner");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		Destroy(gameObject);
+		spawner.SendMessage ("spawnEgg");
+	}
+
+}
---
+++ ProjectTangoCurvedLight/Assets/spawnEggs.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spawnEggs : MonoBehaviour {
+
+	public GameObject egg;
+	public GameObject[] spawnPoints;
+
+	private int pointIndex = -1;
+	private int lastPoint = -1;
+
+	void Start() {
+			spawnPoints = GameObject.FindGameObjectsWithTag("SpawnPoint");
+
+		spawnEgg ();
+	}
+
+	void spawnEgg()
+	{
+		if (spawnPoints.Length > 1) {
+			while (pointIndex == lastPoint) {
+				pointIndex = Random.Range (0, (spawnPoints.Length - 1));
+			}
+			lastPoint = pointIndex;
+			Instantiate (egg, spawnPoints [pointIndex].transform.position, Quaternion.identity);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
